ae0ae8827f363af3e9c1c2cd7b562928fb289032
Okram Meitei <okrammeitei@Okram-Mac-Mini.local>
2024-12-08 17:26:53 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,99 @@
+This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/app/api-reference/cli/create-next-app).
+
+## Getting Started
+
+First, run the development server:
+
+```bash
+npm run dev
+# or
+yarn dev
+# or
+pnpm dev
+# or
+bun dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+
+You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
+
+This project uses [`next/font`](https://nextjs.org/docs/app/building-your-application/optimizing/fonts) to automatically optimize and load [Geist](https://vercel.com/font), a new font family for Vercel.
+
+## Learn More
+
+To learn more about Next.js, take a look at the following resources:
+
+- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
+- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
+
+You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js) - your feedback and contributions are welcome!
+
+## Deploy on Vercel
+
+The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
+
+Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/building-your-application/deploying) for more details.
+
+<!-- Clinton Edition ========================================================= -->
+<!-- 20241208================================== -->
+
+install
+npm install @tiptap/react@2.10.3 @tiptap/pm@2.10.3 @tiptap/starter-kit@2.10.3 --legacy-peer-deps
+
+<!-- ========================================================== -->
+
+npm i @tiptap/extension-task-list@2.10.3 @tiptap/extension-task-item@2.10.3 --legacy-peer-deps
+
+<!-- =========================== -->
+
+npm install @tiptap/extension-table@2.10.3 @tiptap/extension-table-row@2.10.3 @tiptap/extension-table-header@2.10.3 @tiptap/extension-table-cell@2.10.3 --legacy-peer-deps
+
+<!-- ===================== -->
+
+npm install @tiptap/extension-image@2.10.3 --legacy-peer-deps
+
+<!-- ========================= -->
+
+npm install tiptap-extension-resize-image --legacy-peer-deps
+
+<!--  ============================== -->
+
+npm i zustand@5.0.2 --legacy-peer-deps
+
+<!-- ================================= -->
+
+npm install @tiptap/extension-underline@2.10.3 --legacy-peer-deps
+
+<!-- ================================= -->
+
+npm install @tiptap/extension-text-style@2.10.3 @tiptap/extension-font-family@2.10.3 --legacy-peer-deps
+
+<!-- ================================ -->
+
+2:07
+
+<!-- ================================= -->
+
+npm install @tiptap/extension-heading@2.10.3 --legacy-peer-deps
+
+<!-- ============================= -->
+
+2:18:49
+
+<!-- ============================== -->
+
+npm i react-color@2.19.3 --legacy-peer-deps
+
+<!-- ========================= -->
+
+npm install @tiptap/extension-highlight@2.10.3 --legacy-peer-deps
+
+<!-- ================================= -->
+
+npm install @tiptap/extension-color@2.10.3 --legacy-peer-deps
+
+<!-- ========================== -->
+
+npm i --save-dev @types/react-color@3.0.12 --legacy-peer-deps
+# docx-editor
---
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -0,0 +1,323 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { Separator } from "@/components/ui/separator";
+import { useEditorStore } from "@/store/use-editor-store";
+import {
+  BoldIcon,
+  ChevronDownIcon,
+  HighlighterIcon,
+  ItalicIcon,
+  ListTodoIcon,
+  LucideIcon,
+  MessageSquareCodeIcon,
+  PrinterIcon,
+  Redo2Icon,
+  RemoveFormattingIcon,
+  SpellCheckIcon,
+  UnderlineIcon,
+  Undo2Icon,
+} from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  // DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Level } from "@tiptap/extension-heading";
+// import Color from "@tiptap/extension-color";
+import { ColorResult, SketchPicker } from "react-color";
+
+const HighlightColorButton = () => {
+  const { editor } = useEditorStore();
+
+  const onChange = (color: ColorResult) => {
+    editor?.chain().focus().setHighlight({ color: color.hex }).run();
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <button className="h-7 min-w-7 shrink-0 flex flex-col items-center justify-center rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm">
+          <HighlighterIcon className="size-4" />
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="p-0">
+        <SketchPicker onChange={onChange} />
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+const TextColorButton = () => {
+  const { editor } = useEditorStore();
+
+  const value = editor?.getAttributes("textStyle").color || "#000000";
+
+  const onChange = (color: ColorResult) => {
+    editor?.chain().focus().setColor(color.hex).run();
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <button className="h-7 min-w-7 shrink-0 flex flex-col items-center justify-center rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm">
+          <span className="text-xs">A</span>
+          <div className="h-0.5 w-full" style={{ backgroundColor: value }} />
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="p-0">
+        <SketchPicker color={value} onChange={onChange} />
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+const HeadingLevelButton = () => {
+  const { editor } = useEditorStore();
+
+  const headings = [
+    { label: "Normal text", value: 0, fontSize: "16px" },
+    { label: "Heading 1", value: 1, fontSize: "32px" },
+    { label: "Heading 2", value: 2, fontSize: "28px" },
+    { label: "Heading 3", value: 3, fontSize: "24px" },
+    { label: "Heading 4", value: 4, fontSize: "20px" },
+    { label: "Heading 5", value: 5, fontSize: "18px" },
+    { label: "Heading 6", value: 6, fontSize: "16px" },
+    // { label: "Subheading", value: 7, fontSize: "14px" },
+    // { label: "Body Text", value: 8, fontSize: "12px" },
+    // { label: "Small Text", value: 9, fontSize: "10px" },
+  ];
+  const getCurrentHeading = () => {
+    for (let level = 1; level <= 5; level++) {
+      if (editor?.isActive("heading", { level })) {
+        return `heading ${level}`;
+      }
+    }
+
+    return "Normal text";
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <button className="h-7 min-w-7 shrink-0 flex items-center justify-center rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm">
+          <span className="truncate">{getCurrentHeading()}</span>
+          <ChevronDownIcon className="ml-2 size-4 shrink-0" />
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="p-1 flex flex-col gap-y-1">
+        {headings.map(({ label, value, fontSize }) => (
+          <button
+            key={value}
+            style={{ fontSize }}
+            onClick={() => {
+              if (value === 0) {
+                editor?.chain().focus().setParagraph().run();
+              } else {
+                editor
+                  ?.chain()
+                  .focus()
+                  .toggleHeading({ level: value as Level })
+                  .run();
+              }
+            }}
+            className={cn(
+              "flex items-center gap-x-2 px-2 py-1 rounded-sm hover:bg-neutral-200/80",
+              (value === 0 && !editor?.isActive("heading")) ||
+                (editor?.isActive("heading", { level: value }) &&
+                  "bg-neutral-200/80")
+            )}
+          >
+            {label}
+          </button>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+const FontFamilyButton = () => {
+  const { editor } = useEditorStore();
+
+  const fonts = [
+    { label: "Arial", value: "Arial" }, // Basic browser support fonts
+    { label: "Times New Roman", value: "Times New Roman" },
+    { label: "Courier New", value: "Courier New" },
+    { label: "Georgia", value: "Georgia" },
+    { label: "Verdana", value: "Verdana" },
+    { label: "Tahoma", value: "Tahoma" }, // Another sans-serif font
+    { label: "Trebuchet MS", value: "Trebuchet MS" }, // A clean, sans-serif font
+    { label: "Impact", value: "Impact" }, // Bold, strong font
+    { label: "Comic Sans MS", value: "Comic Sans MS" }, // Playful font
+    { label: "Lucida Console", value: "Lucida Console" }, // Monospace font
+    { label: "Lucida Sans Unicode", value: "Lucida Sans Unicode" }, // Sans-serif, clean font
+    { label: "Arial Black", value: "Arial Black" }, // Heavier version of Arial
+    { label: "Helvetica", value: "Helvetica" }, // Similar to Arial, popular sans-serif font
+    { label: "Palatino Linotype", value: "Palatino Linotype" }, // Elegant serif font
+    { label: "Book Antiqua", value: "Book Antiqua" }, // Serif font similar to Palatino
+    { label: "Courier", value: "Courier" }, // Another monospace font
+    { label: "MS Sans Serif", value: "MS Sans Serif" }, // Old, basic sans-serif font
+    { label: "MS Serif", value: "MS Serif" }, // Basic serif font
+  ];
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <button className="h-7 w-[120px] shrink-0 flex items-center justify-between rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm">
+          <span className="truncate">
+            {editor?.getAttributes("textStyle").fontFamily || "Arial"}
+          </span>
+          <ChevronDownIcon className="ml-2 size-4 shrink-0" />
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="p-1 flex flex-col gap-y-1">
+        {fonts.map(({ label, value }) => (
+          <button
+            onClick={() => editor?.chain().focus().setFontFamily(value).run()}
+            key={value}
+            className={cn(
+              "flex items-center gap-x-2 px-2 py-1 rounded-sm hover:bg-neutral-200/80",
+              editor?.getAttributes("textStyle").fontfamily === value &&
+                "bg-neutral-200/80"
+            )}
+            style={{ fontFamily: value }}
+          >
+            <span className="text-sm">{label}</span>
+          </button>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+interface ToolbarButtonProps {
+  onClick?: () => void;
+  isActive?: boolean;
+  icon: LucideIcon;
+}
+
+const ToolbarButton = ({
+  onClick,
+  isActive,
+  icon: Icon,
+}: ToolbarButtonProps) => {
+  return (
+    <button
+      onClick={onClick}
+      className={cn(
+        "text-sm h-7 min-w-7 flex items-center justify-center rounded-sm hover:bg-neutral-200/80",
+        isActive && "bg-neutral-200/80"
+      )}
+    >
+      <Icon className="size-4" />
+    </button>
+  );
+};
+
+export const ToolBar = () => {
+  const { editor } = useEditorStore();
+
+  const sections: {
+    label: string;
+    icon: LucideIcon;
+    onClick: () => void;
+    isActive?: boolean;
+  }[][] = [
+    [
+      {
+        label: "Undo",
+        icon: Undo2Icon,
+        onClick: () => editor?.chain().focus().undo().run(),
+      },
+      {
+        label: "Redo",
+        icon: Redo2Icon,
+        onClick: () => editor?.chain().focus().redo().run(),
+      },
+      {
+        label: "Print",
+        icon: PrinterIcon,
+        onClick: () => window.print(),
+      },
+      {
+        label: "Spell Check",
+        icon: SpellCheckIcon,
+        onClick: () => {
+          const current = editor?.view.dom.getAttribute("spellcheck");
+          editor?.view.dom.setAttribute(
+            "spellcheck",
+            current === "false" ? "ture" : "false"
+          );
+        },
+      },
+    ],
+    [
+      {
+        label: "Bold",
+        icon: BoldIcon,
+        isActive: editor?.isActive("bold"),
+        onClick: () => editor?.chain().focus().toggleBold().run(),
+      },
+      {
+        label: "Italic",
+        icon: ItalicIcon,
+        isActive: editor?.isActive("italic"),
+        onClick: () => editor?.chain().focus().toggleItalic().run(),
+      },
+      {
+        label: "Underline",
+        icon: UnderlineIcon,
+        isActive: editor?.isActive("underline"),
+        onClick: () => editor?.chain().focus().toggleUnderline().run(),
+      },
+    ],
+    [
+      {
+        label: "Commemt",
+        icon: MessageSquareCodeIcon,
+        onClick: () => console.log("TODO:Comment"),
+        isActive: false, //TODO: Enable this functionlity
+      },
+      {
+        label: "List Todo",
+        icon: ListTodoIcon,
+        onClick: () => editor?.chain().focus().toggleTaskList().run(),
+        isActive: editor?.isActive("taskList"),
+      },
+      {
+        label: "Remove Formatting",
+        icon: RemoveFormattingIcon,
+        onClick: () => editor?.chain().focus().unsetAllMarks().run(),
+      },
+    ],
+  ];
+  return (
+    <div className="bg-[#F1F4F9] px-2.5 py-0.5 rounded-[24px] min-h-[40px] flex items-center gap-x-0.5 overflow-x-auto">
+      {sections[0].map((item) => (
+        <ToolbarButton key={item.label} {...item} />
+      ))}
+      <Separator orientation="vertical" className="h-6 bg-neutral-300 " />
+      <FontFamilyButton />
+      <Separator orientation="vertical" className="h-6 bg-neutral-300 " />
+      <HeadingLevelButton />
+      <Separator orientation="vertical" className="h-6 bg-neutral-300 " />
+      {/* TODO: Font size */}
+      <Separator orientation="vertical" className="h-6 bg-neutral-300 " />
+      {sections[1].map((item) => (
+        <ToolbarButton key={item.label} {...item} />
+      ))}
+      <TextColorButton />
+      <HighlightColorButton />
+      <Separator orientation="vertical" className="h-6 bg-neutral-300 " />
+      {/* TODO: Link*/}
+      {/* TODO: Image*/}
+      {/* TODO: Align*/}
+      {/* TODO: Lin height*/}
+      {/* TODO: List*/}
+      {sections[2].map((item) => (
+        <ToolbarButton key={item.label} {...item} />
+      ))}
+    </div>
+  );
+};
