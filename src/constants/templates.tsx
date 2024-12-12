83db0463cae930aefaf96eb68698e005d2faae8c
Oinam Clinton Singh <oinamclinton.co@gmail.com>
2024-12-12 23:46:52 +0530

    local commit changes before merging

---
+++ src/constants/templates.tsx
@@ -0,0 +1,12 @@
+export const templates = [
+  {
+    id: "upload",
+    label: "Upload A Document",
+    imageUrl: "/upload.svg",
+  },
+  {
+    id: "blank",
+    label: "Blank Document",
+    imageUrl: "/blank-document.svg",
+  },
+];
